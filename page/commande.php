aecb3d504d809ab812d57abb71bc57d22469135b
Romain <romain.bourgeois22090@gmail.com>
2019-05-14 21:22:31 +0200

    commande

--- page/commande.php
+++ page/commande.php
@@ -1,58 +1,6 @@
 <?php
 session_start();
 include_once("fonctions-panier.php");
-$erreur = false;
-
-$action = (isset($_POST['action'])? $_POST['action']:  (isset($_GET['action'])? $_GET['action']:null )) ;
-if($action !== null)
-{
-   if(!in_array($action,array('ajout', 'suppression', 'refresh')))
-   $erreur=true;
-
-   //récuperation des variables en POST ou GET
-   $l = (isset($_POST['l'])? $_POST['l']:  (isset($_GET['l'])? $_GET['l']:null )) ;
-   $p = (isset($_POST['p'])? $_POST['p']:  (isset($_GET['p'])? $_GET['p']:null )) ;
-   $q = (isset($_POST['q'])? $_POST['q']:  (isset($_GET['q'])? $_GET['q']:null )) ;
-
-   //Suppression des espaces verticaux
-   $l = preg_replace('#\v#', '',$l);
-   //On verifie que $p soit un float
-   $p = floatval($p);
-
-   //On traite $q qui peut etre un entier simple ou un tableau d'entier
-    
-   if (is_array($q)){
-      $QteArticle = array();
-      $i=0;
-      foreach ($q as $contenu){
-         $QteArticle[$i++] = intval($contenu);
-      }
-   }
-   else
-   $q = intval($q);
-    
-}
-
-if (!$erreur){
-   switch($action){
-      Case "ajout":
-         ajouterArticle($l,$q,$p);
-         break;
-
-      Case "suppression":
-         supprimerArticle($l);
-         break;
-      Case "refresh" :
-         for ($i = 0 ; $i < count($QteArticle) ; $i++)
-         {
-            modifierQTeArticle($_SESSION['panier']['libelleProduit'][$i],round($QteArticle[$i]));
-         }
-         break;
-
-      Default:
-         break;
-   }
-}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -74,6 +22,21 @@ if (!$erreur){
     </style>
 </head>
 <body>
+ <script type="text/javascript">
+    var pdfdoc = new jsPDF();
+    var specialElementHandlers = {
+            "#ignoreContent": function(element, renderer) {
+        return true;
+        }
+    };
+    $(document).ready(function(){
+        $('#gpdf').click(function(){
+        pdfdoc.fromHTML($("#PDFcontent").html(), 10, 10, {‘width’: 110, ‘elementHandlers’: specialElementHandlers
+
+    });
+pdfdoc.save("commande.pdf");
+    });});
+</script>
   <div class="container">
     <header class="col-xs-12 mt-5 pull-right">
         <img src="../assets/image/logo.png" width="100%" class="header">
@@ -152,21 +115,6 @@ if (!$erreur){
    ?>
     </footer>
 </div>
-<script type="text/javascript">
-    var pdfdoc = new jsPDF();
-    var specialElementHandlers = {
-            "#ignoreContent": function(element, renderer) {
-        return true;
-        }
-    };
-    $(document).ready(function(){
-        $('#gpdf').click(function(){
-        pdfdoc.fromHTML($("#PDFcontent").html(), 10, 10, {‘width’: 110, ‘elementHandlers’: specialElementHandlers
-
-    });
-pdfdoc.save("commande.pdf");
-    });});
-</script>
 <script src="../assets/js/jquery.js"></script>
 <script src="../assets/js/popper.min.js"></script> 
 <script src="../assets/js/bootstrap.min.js"></script>
